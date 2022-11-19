832b268ad05a664720879a6119562c64c06a7bc4
henry-dura <henryikemefuna@gmail.com>
2022-11-19 08:22:02 +0100

    automatically adding items to table achieved

--- javascript.js
+++ javascript.js
@@ -1,20 +1,24 @@
+const table = document.querySelector('table');
+const addButton = document.querySelector('#add-btn');
+const submiter = document.querySelector('#submit');
+const deleteButton = document.querySelector('.delete');
+
+
+// const deleteButton = document.createElement('button');
+
+// deleteButton.innerText = 'DELETE';
+// deleteButton.classList.add('delete');
+
 
 
 let myLibrary = [
     {
-        name: 'Half of yellow son',
+        name: 'Half of the yellow son',
         author: 'Chimamanda',
     },
 
-    {
-        name:'Purple Hibiscus',
-        author: 'Chimamanda',
-    },
-
-    {
-        name: 'Men behind the Mask',
-        author: 'Prof. Orabueze',
-    }
+    
+  
 ];
 
 function Book(name, author){
@@ -23,17 +27,71 @@ function Book(name, author){
 
 }
 
-// let book1 = new Book('Half of yellow son', 'Chimamanda');
-// let book2 = new Book('Purple Hibiscus', 'Chimamanda')
 
 
-function addBookToLibrary(){
-    for(let bookDetails of myLibrary){
-        console.log(bookDetails.name);
+
+
+
+addButton.addEventListener('click', () =>{
+    document.querySelector('form').style.display = 'block';
+})
+
+
+submiter.addEventListener('click',submitActions)
+
+function submitActions(){
+    const bookName = document.querySelector('#bookName');
+    const author = document.querySelector('#author');
+    
+    if(bookName.value != '' && author.value != ''){
+        
+        myLibrary.push({name:bookName.value, author: author.value});
+
+        while(table.rows.length > 1) {
+            table.deleteRow(1);
+          }
+
+        addBookToLibrary();
+       
+        console.log(bookName.value);
+        console.log(author.value);
+        document.querySelector('form').style.display = 'none';
+        bookName.value = ''; author.value='';
+        event.preventDefault();
     }
+
+   
+
+   
 
 
 }
 
 
-addBookToLibrary()
+
+
+
+function addBookToLibrary(){
+
+    for(let eachBookDetail of myLibrary){
+
+        const row = table.insertRow(1);
+        const cell1 = row.insertCell(0);
+        const cell2 = row.insertCell(1);
+        const cell3 = row.insertCell(2);
+        cell1.innerHTML =eachBookDetail.name ;
+        cell2.innerHTML = eachBookDetail.author;
+        cell3.innerHTML = '<button class="delete" onClick="deleting()"> DELETE</button>';
+    }
+    
+    
+}
+
+
+function deleting(){
+    console.log('deleting');
+}
+
+
+addBookToLibrary();
+console.log(table.rows.length)
